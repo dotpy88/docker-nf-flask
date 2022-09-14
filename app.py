1fa61fee5a5e0e54a353f2d723231a929a96d81e
dotpy88 <tsmtbs07@gmail.com>
2022-09-14 12:40:31 -0500

    added data handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, requests
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
 requests.packages.urllib3.disable_warnings()
@@ -14,10 +14,14 @@ def nflookup():
     password = os.getenv('nde_password')
     nde_api_url = os.getenv('nde_url')
     lookup = request.args.get('lookup')
+    error = "Error returned from API"
     if not lookup: abort(404)
     try:
         response = requests.request('GET', nde_api_url + lookup, auth=HTTPBasicAuth(username,password),verify=False)
-        return response.json()
+        resp = response.json().get('data')
+        api_type = resp.get('api_type')
+        if not api_type: return error
+        return render_template(f'template-{api_type}.html',resp=resp)
     except Exception as e:
         return str(e)
  
